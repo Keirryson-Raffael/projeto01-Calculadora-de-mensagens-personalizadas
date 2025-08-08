aa1f6d8c5af76f00b314b759a6ed57ff2cb49d98
Keirryson-Raffael <keirryson.soares@aluno.ce.gov.br>
2025-08-08 14:42:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const nome = "Frederico"
+let idade = 28
+let cidade = "Belo Horizonte"
+let profissão = "Dramaturgo"
+let anoAtual = 2025
+let salario = 68567
+let bonus = 30271
+
+let anoNascimento = anoAtual - idade
+let totalMensal = salario + bonus
+let totalAno = totalMensal * 12
+let totalAnualD = totalAno * 2
+
+alert(`========= Relatório do Usuario =========
+    Nome: ${nome}
+    Idade: ${idade}
+    Ano de nascimento: ${anoNascimento}
+    Cidade: ${cidade}
+    Profissão: ${profissão}
+    Salário U$${salario}
+    Bônus: U$${bonus}
+    Total mensal: U$${totalMensal}
+    Total Ano: U$${totalAno}
+    Total Anual (Dobrado): U$${totalAnualD}
+    =============================================`)
